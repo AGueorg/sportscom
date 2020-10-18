59b38fb917e6fa61229ce2b8de5b8e3fe10067be
AGueorg <aneta.gueorguieva@icloud.com>
2020-10-18 18:02:24 +0200

    End of section 4

---
+++ API/DTOs/ProductToReturnDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace API.DTOs
+{
+    public class ProductToReturnDTO
+    {
+        public int Id { get; internal set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+        public string ProductType { get; set; }
+        public string ProductBrand { get; set; }
+
+    }
+}
