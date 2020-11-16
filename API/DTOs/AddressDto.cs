ce86ac68d81a80d76ad5e618d54857937f6b74f3
AGueorg <aneta.gueorguieva@icloud.com>
2020-11-17 00:47:05 +0100

    End of Section 15

---
+++ API/DTOs/AddressDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace API.DTOs
+{
+    public class AddressDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+    }
+}
