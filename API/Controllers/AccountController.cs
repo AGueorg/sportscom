280ea96a0427039163a96ce08dac1ca54c1f7287
AGueorg <aneta.gueorguieva@icloud.com>
2020-11-17 12:50:01 +0100

    End of section 16

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -61,7 +61,7 @@ namespace API.Controllers
 
         [Authorize]
         [HttpPut("address")]
-        public async Task<ActionResult<AppUser>> UpdateUserAddress(AddressDto address)
+        public async Task<ActionResult<AddressDto>> UpdateUserAddress(AddressDto address)
         {
             var user = await _userManager.FindUserByClaimsPrincipleWithAddressAsync(HttpContext.User);
 
@@ -99,6 +99,13 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if (CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse
+                {
+                    Errors = new[] { "Email address is in use" }
+                });
+            }
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
