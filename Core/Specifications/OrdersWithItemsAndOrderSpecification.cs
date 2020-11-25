9f75d03633428087dd259b0d0f1c37dc1ba49bc7
AGueorg <aneta.gueorguieva@icloud.com>
2020-11-25 14:57:08 +0100

    End of section 18

---
+++ Core/Specifications/OrdersWithItemsAndOrderSpecification.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq.Expressions;
+using Core.Entities.OrderAggregate;
+
+namespace Core.Specifications
+{
+    public class OrdersWithItemsAndOrderSpecification : BaseSpecification<Order>
+    {
+        public OrdersWithItemsAndOrderSpecification(string email) : base(o => o.BuyerEmail == email)
+        {
+            AddInclude(o => o.OrderItems);
+            AddInclude(o => o.DeliveryMethod);
+            AddOrderByDescending(o => o.OrderDate);
+        }
+
+        public OrdersWithItemsAndOrderSpecification(int id, string email) : base(o => o.Id == id && o.BuyerEmail == email)
+        {
+            AddInclude(o => o.OrderItems);
+            AddInclude(o => o.DeliveryMethod);
+        }
+    }
+}
